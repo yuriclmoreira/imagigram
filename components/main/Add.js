d111800cba781267155107c284a5adbc392e0fa3
Yuri Moreira <yuriclmoreira@terra.com.br>
2023-04-05 17:42:05 -0300

    Refactoring due to camera issue

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,8 +1,40 @@
-import React from "react";
-import { View, Text } from "react-native";
-const Add = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Add</Text>
-  </View>
-);
-export default Add;
+import React, { useState } from "react";
+import { Camera, CameraType } from "expo-camera";
+import { Text, Button, View } from "react-native";
+
+export default function Add() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <Camera style={{ flex: 1, aspectRatio: 1 }} type={type} ratio={"1:1"} />
+        <Button title="Flip Camera" onPress={toggleCameraType} />
+      </View>
+    </View>
+  );
+}
