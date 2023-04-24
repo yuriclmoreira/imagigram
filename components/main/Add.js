876998a8b123596a4de6f3133239fb601a20cc61
Yuri Moreira <yuriclmoreira@terra.com.br>
2023-04-24 15:16:09 -0300

    feat: Implemented Take Picture button

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,39 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Camera, CameraType } from "expo-camera";
-import { Text, Button, View } from "react-native";
+import { View, Image } from "react-native";
+import { Text, Button } from "react-native-paper";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Add() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState();
   const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
 
-  if (!permission) {
-    // Camera permissions are still loading
+  useEffect(() => {
+    (async () => {
+      const cameraRequest = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraRequest.status === "granted");
+
+      const galleryRequest =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryRequest.status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  if (hasCameraPermission === null || hasGalleryPermission === null) {
     return <View />;
   }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet
-    return (
-      <View style={{ flex: 1 }}>
-        <Text style={{ textAlign: "center" }}>
-          We need your permission to show the camera
-        </Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
-  }
-
-  function toggleCameraType() {
-    setType((current) =>
-      current === CameraType.back ? CameraType.front : CameraType.back
-    );
+  if (hasCameraPermission === false || hasGalleryPermission === null) {
+    return <Text>No access to camera</Text>;
   }
 
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 1, flexDirection: "row" }}>
-        <Camera style={{ flex: 1, aspectRatio: 1 }} type={type} ratio={"1:1"} />
-        <Button title="Flip Camera" onPress={toggleCameraType} />
+        {image ? (
+          <Image source={{ uri: image }} style={{ flex: 1 }} />
+        ) : (
+          <Camera
+            ref={(ref) => setCamera(ref)}
+            style={{ flex: 1, aspectRatio: 1 }}
+            type={type}
+            ratio={"1:1"}
+          />
+        )}
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          title="Flip Camera"
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        />
+        <Button icon="camera-plus-outline" onPress={() => takePicture()}>
+          Take Picture
+        </Button>
+        <Button icon="image-album" onPress={() => pickImage()}>
+          Select from the Gallery
+        </Button>
       </View>
     </View>
   );
